9b5e9dbec90c2ef2b43e6f153ba79b9fc254ab9e
Mohammed Anwar Sadat Mamudu <anwarsadat.d2@gmail.com>
2022-07-01 19:59:07 -0700

    Variables, if, else, while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -10,8 +10,8 @@ int main(void)
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
-       		putchar('\n');
+	putchar(c);
+       	putchar('\n');
 
 	return (0);
 }
