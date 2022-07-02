515245618cbe80006b95ab9e9dd672cd7dc19ea6
Mohammed Anwar Sadat Mamudu <anwarsadat.d2@gmail.com>
2022-07-01 19:47:23 -0700

    Variables, if, else, while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all unit digits
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 0; d <= 9; d++)
+	printf("%i", d);
+	putchar('\n');
+
+
+	return (0);
+}
+
