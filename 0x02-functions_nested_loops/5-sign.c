f9b389ad9d052bd160665b55dd09e56e8312aefe
Mohammed Anwar Sadat Mamudu <anwarsadat.d2@gmail.com>
2022-07-05 14:44:51 -0700

    Write a function that checks for alphabetic character.

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,8 +5,6 @@
  @n: The integer under consideration
 
  Return: Returns 1 and prints + if n is greater than zero
- 	Returns 0 and prints 0 if n is zero
-	Returns -1 and prints - if n is less than zero
 */
 
 int print_sign(int n)
@@ -15,17 +13,17 @@ int print_sign(int n)
 
 	{
 		_putchar(43);
-		return(1);
+		return (1);
 	
 	}
 	else if ( n <= -1)
 	{
 		_putchar(45);
-		return(-1);
+		return (-1);
 	}
-	else 
+	else
 	{
 		_putchar(48);
-		return(0);
+		return (0);
 	}
 }
