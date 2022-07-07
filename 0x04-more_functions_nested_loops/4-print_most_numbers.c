2eba5bfb98ee744dd625e263518c78223bf796a7
Mohammed Anwar Sadat Mamudu <anwarsadat.d2@gmail.com>
2022-07-07 13:26:35 -0700

    Adds a program to print numbers except 0 and 9

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,15 +3,13 @@
 /**
   * print_most_numbers - Print the numbers since 0 up to 9
   *
-  * Description: Prints the numbers excluding 2 and 4
-  *
   * Return: The numbers since 0 up to 9
   */
 void print_most_numbers(void)
 {
-	int x = 0;
+	int x;
 
-	for (; x <= 9; x++)
+	for (x = 0; x <= 9; x++)
 	{
 		if (x == 2 || x == 4)
 		{
