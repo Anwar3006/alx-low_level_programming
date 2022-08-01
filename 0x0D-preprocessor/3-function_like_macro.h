0d47808a34676496b301dd616e16adb6f1c76622
Mohammed Anwar Sadat Mamudu <anwarsadat.d2@gmail.com>
2022-08-01 07:45:22 -0700

    Preprocessor

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_LIKE_MARCOS_H
+#define FUNCTION_LIKE_MARCOS_H
+
+/**
+ * Header file defines a function-like marcos ABS(x)
+ * that computes the absolute value of a number
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,11 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * Header file the defines the marcos SUM(x, y)
+ * and computes the sum of the values in the parenthesis
+ */
+
+#define SUM(x, y)  ((x) + (y))
+
+#endif
