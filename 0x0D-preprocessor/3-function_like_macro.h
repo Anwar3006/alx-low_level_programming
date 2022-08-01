2268d509e11b05c6ceea54699a2125e40b2573f6
Mohammed Anwar Sadat Mamudu <anwarsadat.d2@gmail.com>
2022-08-01 07:52:38 -0700

    Preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,10 +1,12 @@
 #ifndef FUNCTION_LIKE_MARCOS_H
 #define FUNCTION_LIKE_MARCOS_H
-
-/**
- * Header file defines a function-like marcos ABS(x)
- * that computes the absolute value of a number
+/*
+ * File: 3-function_like_macro.h
+ * 
+ * Desc: Header file that defines a function-like macro ABS(x)
+ *       that computes the absolute value of a number x.
  */
+
 
 #define ABS(x) ((x) < 0 ? (-x) : (x))
 
