0e65998c2243e3b222afa2541eb83f04093870ba
Mohammed Anwar Sadat Mamudu <anwarsadat.d2@gmail.com>
2022-08-01 07:49:59 -0700

    Preprocessor

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -2,8 +2,8 @@
 #define SUM_H
 
 /**
- * Header file the defines the marcos SUM(x, y)
- * and computes the sum of the values in the parenthesis
+ * Desc: Header file the defines the marcos SUM(x, y)
+ * 	and computes the sum of the values in the parenthesis
  */
 
 #define SUM(x, y)  ((x) + (y))
