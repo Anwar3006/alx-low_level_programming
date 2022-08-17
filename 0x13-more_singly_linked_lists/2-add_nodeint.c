26f3c8980fdd1d4b6181b466dddb9c220d04a735
Anwar3006 <anwarsadat.d2@gmail.com>
2022-08-17 13:32:03 +0000

    Singly Linked Lists

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,7 +4,7 @@
 
 /**
  * add_nodeint - Adds a new node at the beginning
- *            of a list_t list.
+ *          of a list_t list.
  * @head: A pointer to the head of the list_t list.
  * @n: The integer to be added to the list_t list.
  *
@@ -28,4 +28,5 @@ listint_t *add_nodeint(listint_t **head, const int n)
     *head = number;
 
     return (number);  
+    
 }
