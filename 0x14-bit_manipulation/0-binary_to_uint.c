748f896d7886701c623419629da6bd7ebff86032
Anwar3006 <anwarsadat.d2@gmail.com>
2022-08-18 15:41:26 +0000

    Bit Manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Convert binary value to unsigned int value
+ * @b: Pointer to string of char 0 and 1
+ * 
+ * Return: Value in Base 10
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+    unsigned int dex = 0, weight = 1;
+    int rem, bin;
+    b = &dex;
+
+    while (bin != 0)
+    {
+        rem = bin % 10;
+        dex = dex + rem * weight;
+        bin = bin / 10;
+        weight = weight * 2;
+    }
+    return (dex);
+}
