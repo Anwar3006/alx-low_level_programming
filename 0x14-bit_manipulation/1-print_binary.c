5bfd2c4ffa9bd596e02152457e931f9cbdcc5afe
Anwar3006 <anwarsadat.d2@gmail.com>
2022-08-18 18:17:41 +0000

    Bit Manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,30 +8,8 @@
   */
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
 
-	divdiv(n);
-}
-
-/**
-  * divdiv - ...
-  * @n: ...
-  *
-  * Return: ...
-  */
-void divdiv(unsigned long int n)
-{
-	if (n < 1)
-		return;
-
-	divdiv(n >> 1);
-
-	if (n & 1)
-		_putchar('1');
-	else
-		_putchar('0');
+	_putchar((n & 1) + '0');
 }
--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,10 +10,11 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-    int n, index, bitStatus;
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
 
-    /* Right shift n, index times and perform bitwise AND with 1 */
-    bitStatus = (n >> index) & 1;
+	if ((n & (1 << index)) == 0)
+		return (0);
 
-    return bitStatus;
+	return (1);
 }
