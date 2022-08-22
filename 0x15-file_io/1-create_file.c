6fddd499626a1305fb1ec1e6bb80ba76227a841a
Anwar3006 <anwarsadat.d2@gmail.com>
2022-08-22 16:29:39 +0000

    File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,44 +1,50 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * create_file : Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
- *
- * Return: If the function fails - -1.
- *Otherwise - 1.
+ * read_textfile - Reads a file and prints it to the POSIX stdout.
+ * @filename: The name of the file that needs to be read.
+ * @letters: Is the number of letters the function should print.
+ * Return: The actual number of letters it could read and print.
  */
-int create_file(const char *filename, char *text_content)
+
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int o, w, count;
-    if (filename == NULL)
-    {
-        return (-1);
-    }
+	int fd;
+	int i;
+	int read_letters;
+	char *buf;
 
-    if (text_content != 0);
-    {
-        text_content = " ";
-    }
-
-	o = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0600);
-    if (o < 0)
+	if (filename == NULL)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 	{
-		if (errno == EEXIST)
+		free(buf);
+		return (0);
+	}
+
+	read_letters = read(fd, buf, letters);
+	if (read_letters == -1)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
+
+	for (i = 0; i < read_letters; i++)
+	{
+		if (write(STDOUT_FILENO, &buf[i], 1) == -1)
 		{
-			o = open(filename, O_WRONLY | O_TRUNC);
-			if (o == -1)
-				return (-1);
+			close(fd);
+			free(buf);
+			return (0);
 		}
-		else
-			return (-1);
+
 	}
-	for (int i = 0; text_content[i] != '\0'; i++)
-	{
-		if (write(o, &text_content[i], 1) == -1)
-			return (-1);
-	}
-	close(o);
-	return (1);
+	close(fd);
+	free(buf);
+	return (read_letters);
 }
