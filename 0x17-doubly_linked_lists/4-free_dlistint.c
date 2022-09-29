4874da2c5b549510ecaf1d6704b40b6ca8e17560
Anwar3006 <anwarsadat.d2@gmail.com>
2022-09-29 19:47:29 +0000

    Doubly Linked Lists

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -6,12 +6,12 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *current = head;
-    while (current->next != NULL)
-    {
-        current = current->next;
-        free(current->prev);
-    }
-    free(current);
-    head = NULL;
+  	dlistint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
 }
