5239d8d5edd25b6aedce3c5010a44f56cf1dc4ca
Anwar3006 <anwarsadat.d2@gmail.com>
2022-10-05 10:09:39 +0000

    Doubly Linked Lists

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,10 +14,17 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
         return NULL;
     
     new_node->n = n;
-    new_node->next = *head;
-    new_node->prev = (*head)->prev;
-    (*head)->prev = new_node;
-    *head = new_node;
+	if (*head)
+	{
+		new_node->next = *head;
+		new_node->prev = (*head)->prev;
+		(*head)->prev = new_node;
+		*head = new_node;
+		return (*head);
+	}
 
-    return (*head);
+	new_node->next = *head;
+	new_node->prev = NULL;
+	*head = new_node;
+	return (*head);
 }
