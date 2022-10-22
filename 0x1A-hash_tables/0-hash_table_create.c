78d1d181a8710db1a23c7b6a24540b179b3c3164
Anwar3006 <anwarsadat.d2@gmail.com>
2022-10-22 13:12:32 +0000

    Data Structures and Algorithms

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,28 +1,27 @@
 #include "hash_tables.h"
 
 /**
- * *hash_table_create - creates a hash table
- * @size: size of hashtable
- * 
- * Return: pointer to newly created hashtable
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
-   hash_table_t *hashtable = malloc(sizeof(hash_table_t) * 1);
-   if (hashtable == NULL)
-        return NULL;
+	hash_table_t *ht;
+	unsigned long int i;
 
-   hashtable->size = size;
-   hashtable->array = malloc(sizeof(hash_node_t *) * size);
-   if (hashtable->array == NULL)
-        return NULL;
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
 
-   unsigned int i;
-   for (i = 0; i < size; i++)
-   {
-    hashtable->array[i] = NULL;
-   }
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 
-    return hashtable;
+	return (ht);
 }
