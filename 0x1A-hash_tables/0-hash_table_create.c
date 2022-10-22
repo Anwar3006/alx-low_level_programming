7fb630816c0ff5ca7e8a48619b730255fc6edff2
Anwar3006 <anwarsadat.d2@gmail.com>
2022-10-22 13:05:03 +0000

    Data Structures and Algorithms

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,20 +9,20 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-   //allocate the hashtable to a pointer
+   /* allocate the hashtable to a pointer */
    hash_table_t *hashtable = malloc(sizeof(hash_table_t) * 1);
    if (hashtable == NULL)
         return NULL;
 
-   //allocate array ponters the quantity determined by @size
+   /* allocate array ponters the quantity determined by @size */
    hashtable->size = size;
    hashtable->array = malloc(sizeof(hash_node_t *) * size);
    if (hashtable->array == NULL)
         return NULL;
 
-   //set each array pointer created to NULL
-   int i = 0;
-   for (; i < size; i++)
+   /* set each array pointer created to NULL */
+   int i;
+   for (i = 0; i < size; i++)
    {
     hashtable->array[i] = NULL;
    }
