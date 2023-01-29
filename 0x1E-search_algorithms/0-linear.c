4ca1f69d360b803ca6ea70a98bca8dbe69978ab0
Anwar3006 <anwarsadat.d2@gmail.com>
2023-01-29 18:10:26 +0000

    Search Algorithm

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Search for a value in an array
+ * 
+ * @array: array to search
+ * @size:  length of array
+ * @value:  value to search for
+ *  
+ * @return - Index if value is found
+ *           -1 if value not available
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if(array[i] == value)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,16 @@
+#ifndef _SEARCHING_
+#define _SEARCHING_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Header File
+ * 
+ */
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+#endif
