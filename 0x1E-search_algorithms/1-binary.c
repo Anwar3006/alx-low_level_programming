c60e2a35c8a025b1c3283d911f733a27d6f7cbf7
Anwar3006 <anwarsadat.d2@gmail.com>
2023-01-29 19:51:12 +0000

    Search Algorithm

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,48 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - Implements Binary search to locate elements of an array
+ * 
+ * @array: Array to use
+ * @size:  Length of array
+ * @value: Value to search
+ * 
+ * Return - Index of found value or -1 if not found 
+ */
+
+int binary_search(int *array, int size, int value)
+{
+    int firstIndex = 0;
+    int lastIndex = size - 1;
+    
+    return binary_search_wrapper(array, value, firstIndex, lastIndex);
+}
+
+int binary_search_wrapper(int *array, int value, int firstIndex, int lastIndex)
+{
+    int midIndex = (firstIndex + lastIndex) / 2;
+    
+    printf("Searching in array: ");
+    for(int i = firstIndex; i <= lastIndex; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+    
+    if (firstIndex > lastIndex) return -1;
+    
+    if(array[midIndex] == value)
+    {
+        return midIndex;
+    }
+    else if (array[midIndex] < value)
+    {
+        firstIndex = midIndex  + 1; 
+        return binary_search_wrapper(array, value, firstIndex, lastIndex);
+    }
+    else
+    {
+        lastIndex = midIndex - 1;
+        return binary_search_wrapper(array, value, firstIndex, lastIndex);
+    }
+}
