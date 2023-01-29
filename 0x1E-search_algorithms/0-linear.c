86f93ceaf2766ca92520129d9d445ab638470a8e
Anwar3006 <anwarsadat.d2@gmail.com>
2023-01-29 20:38:16 +0000

    Search Algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,12 +2,12 @@
 
 /**
  * linear_search - Search for a value in an array
- * 
+ *
  * @array: array to search
  * @size:  length of array
  * @value:  value to search for
- *  
- * @return - Index if value is found
+ *
+ * Return: Index if value is found
  *           -1 if value not available
  */
 
@@ -16,10 +16,10 @@ int linear_search(int *array, size_t size, int value)
 	for (int i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if(array[i] == value)
+		if (array[i] == value)
 		{
-			return i;
+			return (i);
 		}
 	}
-	return -1;
+	return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -2,47 +2,58 @@
 
 /**
  * binary_search - Implements Binary search to locate elements of an array
- * 
+ *
  * @array: Array to use
  * @size:  Length of array
  * @value: Value to search
- * 
- * Return - Index of found value or -1 if not found 
+ *
+ * Return: Index of found value or -1 if not found
  */
 
 int binary_search(int *array, int size, int value)
 {
 	int firstIndex = 0;
 	int lastIndex = size - 1;
-	
-	return binary_search_wrapper(array, value, firstIndex, lastIndex);
+
+	return (binary_search_wrapper(array, value, firstIndex, lastIndex));
 }
 
+/**
+ * binary_search_wrapper - a wrapper function for the binary_search function
+ *
+ * @array: Array to use
+ * @firstIndex:  As name suggests
+ * @lastIndex:  As name suggests
+ * @value: Value to search
+ *
+ * Return: int
+ */
 int binary_search_wrapper(int *array, int value, int firstIndex, int lastIndex)
 {
 	int midIndex = (firstIndex + lastIndex) / 2;
-	
+
 	printf("Searching in array: ");
-	for(int i = firstIndex; i <= lastIndex; i++)
+	for (int i = firstIndex; i <= lastIndex; i++)
 	{
 		printf("%d ", array[i]);
 	}
 	printf("\n");
-	
-	if (firstIndex > lastIndex) return -1;
-	
-	if(array[midIndex] == value)
+
+	if (firstIndex > lastIndex)
+		return (-1);
+
+	if (array[midIndex] == value)
 	{
-		return midIndex;
+		return (midIndex);
 	}
 	else if (array[midIndex] < value)
 	{
-		firstIndex = midIndex  + 1; 
-		return binary_search_wrapper(array, value, firstIndex, lastIndex);
+		firstIndex = midIndex  + 1;
+		return (binary_search_wrapper(array, value, firstIndex, lastIndex));
 	}
 	else
 	{
 		lastIndex = midIndex - 1;
-		return binary_search_wrapper(array, value, firstIndex, lastIndex);
+		return (binary_search_wrapper(array, value, firstIndex, lastIndex));
 	}
 }
