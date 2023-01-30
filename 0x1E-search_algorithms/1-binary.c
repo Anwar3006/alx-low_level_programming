fad447796020bf4ad391e30b93e763609d7be7a4
Anwar3006 <anwarsadat.d2@gmail.com>
2023-01-30 13:25:51 +0000

    Search Algorithm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,63 +1,46 @@
 #include "search_algos.h"
 
-int binary_search_wrapper(int *array, int value, size_t firstIndex, size_t lastIndex);
 /**
- * binary_search - Implements Binary search to locate elements of an array
- *
- * @array: Array to use
- * @size:  Length of array
- * @value: Value to search
- *
- * Return: Index of found value or -1 if not found
- */
-
+  * binary_search - Searches for a value in a sorted array of
+  * integers using the Binary search algorithm
+  *
+  * @array: A pointer to the first element of the array to search in
+  * @size: The number of elements in array
+  * @value: The value to search for
+  *
+  * Return: The first index where value is located
+  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t firstIndex = 0;
-	size_t lastIndex = size - 1;
+	size_t i = 0, low = 0, mid = 0, high = size - 1;
 
-	return (binary_search_wrapper(array, value, firstIndex, lastIndex));
-}
-
-/**
- * binary_search_wrapper - a wrapper function for the binary_search function
- *
- * @array: Array to use
- * @firstIndex:  As name suggests
- * @lastIndex:  As name suggests
- * @value: Value to search
- *
- * Return: The first index where value is located
- */
-
-int binary_search_wrapper(int *array, int value, size_t firstIndex, size_t lastIndex)
-{
-	size_t midIndex = (firstIndex + lastIndex) / 2;
-
-	printf("Searching in array: ");
-
-	size_t i;
-	for (i = firstIndex; i <= lastIndex; i++)
-	{
-		printf("%d ", array[i]);
-	}
-	printf("\n");
-
-	if (firstIndex > lastIndex)
+	if (array == NULL)
 		return (-1);
 
-	if (array[midIndex] == value)
+	while (low <= high)
 	{
-		return (midIndex);
+		mid = (high + low) / 2;
+		i = low;
+
+		printf("Searching in array: ");
+
+		for (; i <= high; ++i)
+		{
+			if (i != low)
+				printf(", %d", array[i]);
+			else
+				printf("%d", array[i]);
+		}
+
+		printf("\n");
+
+		if (array[mid] < value)
+			low = mid + 1;
+		else if (array[mid] > value)
+			high = mid - 1;
+		else
+			return (mid);
 	}
-	else if (array[midIndex] < value)
-	{
-		firstIndex = midIndex  + 1;
-		return (binary_search_wrapper(array, value, firstIndex, lastIndex));
-	}
-	else
-	{
-		lastIndex = midIndex - 1;
-		return (binary_search_wrapper(array, value, firstIndex, lastIndex));
-	}
+
+	return (-1);
 }
