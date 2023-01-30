4b157046de94034ececf13404ebe85ac4b95033d
Anwar3006 <anwarsadat.d2@gmail.com>
2023-01-30 10:25:17 +0000

    Search Algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,7 +13,7 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	for (int i = 0; i < size; i++)
+	for (size_t i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
